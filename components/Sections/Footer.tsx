8aa37fd66558d211872a4fdebf60d333427df14d
Harshalvk <harshalvkhobragade@gmail.com>
2026-06-11 17:34:23 +0530

    style: update footer with new ui style

--- components/Sections/Footer.tsx
+++ components/Sections/Footer.tsx
@@ -1,62 +1,52 @@
-import React from "react";
-import { ModeToggle } from "../ModeToggle";
+import Link from 'next/link';
+import React from 'react';
 
 const Footer = () => {
   return (
-    <section className="font-mono">
-      <div className="w-full flex justify-between items-center">
-        <p className="text-muted-foreground text-sm font-mono">
-          Copyright &#169; 2021 - 2025 Harshal Khobragade
+    <section className="py-4 font-mono">
+      <div className="flex w-full items-center justify-between">
+        <p className="text-muted-foreground font-mono text-sm">
+          Copyright &#169; 2021 - 2026 Harshal Khobragade
         </p>
-        <ModeToggle />
       </div>
-      <div className="w-full flex justify-between mt-4">
+      <div className="mt-4 flex w-full justify-between">
         <div className="flex flex-col">
           <h2 className="text-[18px]">Important Links</h2>
-          <a
-            className="hover:underline underline-offset-4 text-muted-foreground"
-            href="/"
-          >
+          <Link className="text-muted-foreground underline-offset-4 hover:underline" href="/">
             Home
-          </a>
-          <a
-            className="hover:underline underline-offset-4 text-muted-foreground"
-            href="/work"
-          >
+          </Link>
+          <a className="text-muted-foreground underline-offset-4 hover:underline" href="/work">
             Work
           </a>
-          <a
-            className="hover:underline underline-offset-4 text-muted-foreground"
-            href="/blogs"
-          >
+          <a className="text-muted-foreground underline-offset-4 hover:underline" href="/blogs">
             Blog
           </a>
         </div>
         <div className="flex flex-col">
           <h2 className="text-[18px]">Social</h2>
           <a
-            className="hover:underline underline-offset-4 text-muted-foreground"
+            className="text-muted-foreground underline-offset-4 hover:underline"
             href="https://github.com/Harshalvk/"
             target="_blank"
           >
             GitHub
           </a>
           <a
-            className="hover:underline underline-offset-4 text-muted-foreground"
+            className="text-muted-foreground underline-offset-4 hover:underline"
             href="https://x.com/Harshalvk_"
             target="_blank"
           >
             Twitter / X
           </a>
           <a
-            className="hover:underline underline-offset-4 text-muted-foreground"
+            className="text-muted-foreground underline-offset-4 hover:underline"
             href="https://discord.com/users/harsshal."
             target="_blank"
           >
             Discord
           </a>
           <a
-            className="hover:underline underline-offset-4 text-muted-foreground"
+            className="text-muted-foreground underline-offset-4 hover:underline"
             href="https://www.linkedin.com/in/harshalvk/"
             target="_blank"
           >
@@ -65,9 +55,7 @@ const Footer = () => {
         </div>
         <div className="flex flex-col">
           <h2 className="text-[18px]">Other</h2>
-          <a className="hover:underline underline-offset-4 text-muted-foreground">
-            Contact
-          </a>
+          <a className="text-muted-foreground underline-offset-4 hover:underline">Contact</a>
         </div>
       </div>
     </section>
