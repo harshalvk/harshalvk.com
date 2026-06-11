c2dd32353856a8f605fc28b55c8c96ad5e6c8201
Harshalvk <harshalvkhobragade@gmail.com>
2026-06-11 17:34:59 +0530

    style: update my-tech with new ui style

--- components/Sections/MyTechnologies.tsx
+++ components/Sections/MyTechnologies.tsx
@@ -1,35 +1,34 @@
-"use client";
+'use client';
 
-import React from "react";
-import TechBadge from "../TechBadge";
+import React from 'react';
 import {
-  myTechLogos,
-  testAni,
   frameworksAndLibraries,
   backendAndDatabases,
   devopsAndCloud,
   programmingLanguagesAndOthers,
-} from "@/components/constants";
-import Title from "../Title";
-import { motion } from "motion/react";
-import { Separator } from "../ui/separator";
-import MotionBadge from "../MotionBadge";
+} from '@/components/constants';
+import { Title } from '../Typography';
+import MotionBadge from '../MotionBadge';
+import SectionBorders from '../shared/SectionBorders';
 
 const MyTechnologies = () => {
   return (
-    <section className="my-5">
-      <Title text="Technologies I use." />
-      <p className="text-muted-foreground font-mono leading-5">
-        Over the years, I have worked with a variety of technologies. Here are
-        some of the technologies I have experience with:
-      </p>
-      <MotionBadge badges={frameworksAndLibraries} />
-      <MotionBadge badges={backendAndDatabases} reverse={true} />
-      <MotionBadge badges={devopsAndCloud} />
-      <MotionBadge badges={programmingLanguagesAndOthers} reverse={true} />
-      <p className="text-center font-mono text-muted-foreground mt-1">
-        ...and many more!
-      </p>
+    <section className="border-border relative border border-t-0 border-b-0">
+      <SectionBorders />
+      <div className="bg-hatching screen-line-top screen-line-bottom h-4 w-full" />
+      <Title text="Technologies I Use." className="px-4 py-1" />
+      <div className="bg-hatching screen-line-top screen-line-bottom h-4 w-full" />
+      <div className="p-4">
+        <p className="text-muted-foreground font-mono leading-5">
+          Over the years, I have worked with a variety of technologies. Here are some of the
+          technologies I have experience with:
+        </p>
+        <MotionBadge badges={frameworksAndLibraries} />
+        <MotionBadge badges={backendAndDatabases} reverse={true} />
+        <MotionBadge badges={devopsAndCloud} />
+        <MotionBadge badges={programmingLanguagesAndOthers} reverse={true} />
+        <p className="text-muted-foreground mt-1 text-center font-mono">...and many more!</p>
+      </div>
     </section>
   );
 };
