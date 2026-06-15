76b4ec0708709bea177d9fa344ab7268fea311d9
Harshalvk <harshalvkhobragade@gmail.com>
2026-06-15 15:46:50 +0530

    refactor: move tooltip provider to AppProvider

--- src/components/Providers/AppProvider.tsx
+++ src/components/Providers/AppProvider.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from '@/components/ui/sonner';
 import { KeyboardShortcuts } from '@/components/keyboard-shortcuts';
 import { ProgressProvider } from '@bprogress/next/app';
+import { TooltipProvider } from '../ui/tooltip';
 
 const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient();
@@ -13,23 +14,25 @@ const AppProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider
-          attribute={'class'}
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <ProgressProvider
-            color="var(--foreground)"
-            height="2px"
-            delay={500}
-            options={{ showSpinner: false }}
+        <TooltipProvider>
+          <ThemeProvider
+            attribute={'class'}
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
           >
-            <Toaster />
-            {children}
-            <KeyboardShortcuts />
-          </ProgressProvider>
-        </ThemeProvider>
+            <ProgressProvider
+              color="var(--foreground)"
+              height="2px"
+              delay={500}
+              options={{ showSpinner: false }}
+            >
+              <Toaster />
+              {children}
+              <KeyboardShortcuts />
+            </ProgressProvider>
+          </ThemeProvider>
+        </TooltipProvider>
       </QueryClientProvider>
     </>
   );
