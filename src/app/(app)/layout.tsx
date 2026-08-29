8ca8d0e7b586c9cfc5165dae393eca79a943d89f
Harshalvk <harshalvkhobragade@gmail.com>
2026-08-29 22:54:31 +0530

    feat: add ScrollToTop component for improved navigation

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,11 @@
 import Navbar from '@/components/shared/Navbar';
 import Footer from '@/modules/portfolio/components/Sections/Footer';
+import dynamic from 'next/dynamic';
 import React from 'react';
+
+const ScrollToTop = dynamic(() =>
+  import('@/components/scroll-to-top').then((mod) => mod.ScrollToTop)
+);
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -9,6 +14,7 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="bg-background h-19" />
       <main className="flex flex-2 flex-col">{children}</main>
       <Footer />
+      <ScrollToTop />
     </>
   );
 };
